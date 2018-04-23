6fda5c5c61b129ba5375c87bc110d443d940a400
colyk <colyk@yandex.ru>
2018-04-23 22:32:46 +0200

    Minor fix

--- travel_info.py
+++ travel_info.py
@@ -6,20 +6,13 @@ engine = create_engine('sqlite:///travel_info.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-print(session.query(User).filter(User.login == 'colyk' ).all())
+
 
 def log_in(login='colyk', password='1234'):
-    pass_
-    try:
-        pass_ = session.query(User).filter(User.login == login).first().password
-    except Exception:
-        return 1
-    if pass_ == password:
-        # new_user = User(login="colyk", password="1234")
-        # session.add(new_user)
-        # session.commit()
+    if session.query(User).filter(User.login == login, User.password == password).all():
         return 1
     return 0
+
 
 def sign_up(login='colyk', password='1234'):
     if not session.query(User).filter(User.login == login).all():
@@ -34,3 +27,6 @@ def add_comment():
     new_comment = Comment(activity='activity', user=new_user)
     session.add(new_comment)
     session.commit()
+
+
+print(log_in())
